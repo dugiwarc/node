c072639856ad86cb2d39804ac0e5c0e0d97e552a
dugiwarc <gicubotnaru@gmail.com>
2021-03-04 00:38:14 +0100

    finish parsing payloads

--- uptimeMonitorApp/index.js
+++ uptimeMonitorApp/index.js
@@ -2,6 +2,7 @@
 // Dependencies
 const http = require("http");
 const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 // The server should respond to all requests with a string
 const server = http.createServer(function (req, res) {
@@ -21,10 +22,21 @@ const server = http.createServer(function (req, res) {
 	// Get the headers as an object
 	const headers = req.headers;
 
-	console.log(headers);
+	// Get the payload if any
+	const decoder = new StringDecoder("utf-8");
+	let buffer = "";
 
-	// Send the response
-	res.end("Hello");
+	req.on("data", function (data) {
+		buffer += decoder.write(data);
+	});
+
+	req.on("end", function () {
+		buffer += decoder.end();
+
+		res.end("Hello World\n");
+
+		console.log("Buffer", buffer);
+	});
 });
 
 // Start the server and have it listen on port 3000
